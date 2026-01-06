ef901572c1a0ed0ec09a9ef9d7c6a8ae9bd59e3a
a.gushin <a.gushin@eco-fin.eu>
2026-01-06 16:24:47 +0300

    postman_collection.json composer

--- src/config/config.php
+++ src/config/config.php
@@ -5,5 +5,5 @@ return [
     'port' => getenv('DB_PORT') ?: 5432,
     'dbname' => getenv('DB_NAME') ?: 'social_network',
     'user' => getenv('DB_USER') ?: 'postgres',
-    'password' => getenv('DB_PASSWORD') ?: 'postgres'
+    'password' => getenv('DB_PASS') ?: 'postgres'
 ];
